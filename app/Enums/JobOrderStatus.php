7e3f6f1f8c67d2a16aa985345327dba8db39e3f8
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2025-06-23 23:40:10 +0800

    chore: Complete status

--- app/Enums/JobOrderStatus.php
+++ app/Enums/JobOrderStatus.php
@@ -4,32 +4,21 @@ namespace App\Enums;
 
 enum JobOrderStatus: string
 {
-    case ForViewing         = 'for viewing';
-    case ForProposal        = 'for proposal';
-    case ForApproval        = 'for approval';
-    case Successful         = 'successful';
-    case Failed             = 'failed';
-    case Dropped            = 'dropped';
-    case HaulingInProgress  = 'hauling in-progress';
-    case OnHold             = 'on-hold';
-    case Closed             = 'closed';
-    case Completed          = 'completed';
-
-    public function getLabel(): string
-    {
-        return match ($this) {
-            self::ForViewing         => 'For Viewing',
-            self::ForProposal        => 'For Proposal',
-            self::ForApproval        => 'For Approval',
-            self::Successful         => 'Successful',
-            self::Failed             => 'Failed',
-            self::Dropped            => 'Dropped',
-            self::HaulingInProgress  => 'Hauling In-progress',
-            self::OnHold             => 'On-hold',
-            self::Closed             => 'Closed',
-            self::Completed          => 'Completed',
-        };
-    }
+    case ForViewing             = 'for viewing';
+    case ForProposal            = 'for proposal';
+    case ForApproval            = 'for approval';
+    case Successful             = 'successful';
+    case Failed                 = 'failed';
+    case Dropped                = 'dropped';
+    case HaulingInProgress      = 'hauling in-progress';
+    case OnHold                 = 'on-hold';
+    case Closed                 = 'closed';
+    case Completed              = 'completed';
+    case ForPersonnelAssignment = 'for personnel assignment';
+    case ForSafetyInspection    = 'for safety inspection';
+    case ForVerification        = 'for verification';
+    case Verified               = 'verified';
+    case ForAppraisal           = 'for appraisal';
 
     public static function getCancelledStatuses(): array
     {
@@ -38,14 +27,5 @@ enum JobOrderStatus: string
             self::Dropped,
             self::Closed,
         ];
-    }
-
-    public static function getOptions(): array
-    {
-        return array_reduce(self::cases(), function ($carry, $case) {
-            $carry[$case->value] = $case->getLabel();
-
-            return $carry;
-        });
     }
 }
