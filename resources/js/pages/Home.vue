013b67bf746c17f46ccd03354c731bda5f43656c
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2025-06-24 23:29:31 +0800

    Remove head component import

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
-import AppLayout from '@/layouts/AppLayout.vue';
-import { type BreadcrumbItem } from '@/types';
-import { Head } from '@inertiajs/vue3';
-import PlaceholderPattern from '../components/PlaceholderPattern.vue';
+import AppLayout from '@/layouts/AppLayout.vue'
+import { type BreadcrumbItem } from '@/types'
+import PlaceholderPattern from '../components/PlaceholderPattern.vue'
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
--- resources/js/pages/job-orders/Index.vue
+++ resources/js/pages/job-orders/Index.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import AppLayout from '@/layouts/AppLayout.vue'
 import { type BreadcrumbItem, EloquentCollection, JobOrder } from '@/types'
-import { Head } from '@inertiajs/vue3'
 import { ref, watch } from 'vue'
 import { columns } from '@/components/job-orders/columns'
 import JobOrderDataTable from '@/components/job-orders/DataTable.vue'
@@ -26,11 +25,11 @@ watch(() => props.jobOrders, (update) => {
 const breadcrumbs: BreadcrumbItem[] = [
   {
     title: 'Job Orders',
-    href: '#',
+    href: '/job-orders',
   },
   {
     title: 'List',
-    href: '/job_orders',
+    href: '/job-orders',
   }
 ]
 </script>
@@ -39,53 +38,55 @@ const breadcrumbs: BreadcrumbItem[] = [
     <Head title="Job Orders" />
 
     <AppLayout :breadcrumbs="breadcrumbs">
-      <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
-        <div class="flex items-center">
-          <div class="flex flex-col gap-y-1">
-            <h3 class="scroll-m-20 text-3xl font-bold">
-              Job Order List
-            </h3>
-            <p class="text-sm">
-              Manage all existing job orders here.
-            </p>          
+      <div class="px-3 py-3">
+        <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
+          <div class="flex items-center mb-3">
+            <div class="flex flex-col gap-y-1">
+              <h3 class="scroll-m-20 text-3xl font-bold">
+                Job Order List
+              </h3>
+              <p class="text-muted-foreground">
+                You can manage the list of recent active job orders here!
+              </p>
+            </div>
+            <div class="ml-auto">
+              <Link :href="route('job_order.create')">
+                <Button variant="default">
+                  <Plus class="mr-2" />
+                  Create Job Order
+                </Button>
+              </Link>
+            </div>
           </div>
-          <div class="ml-auto">
-            <Link :href="route('job_order.index')">
-              <Button variant="default" class="">
-                <Plus class="mr-2" />
-                Create Job Order
-              </Button>
-            </Link>
-          </div>
-        </div>
-        <Tabs default-value="all" class="w-full">
-          <TabsList class="flex justify-start">
-            <Link :href="route('job_order.index')">
-              <TabsTrigger value="all" class="px-7">
-                All
-              </TabsTrigger>
-            </Link>
-            <Link :href="route('job_order.waste_management')">
-              <TabsTrigger value="waste management" class="px-7">
-                Waste Management
-              </TabsTrigger>
-            </Link>
-            <Link :href="route('job_order.it_service')">
-              <TabsTrigger value="it services" class="px-7">
-                IT Services
-              </TabsTrigger>
-            </Link>
-            <Link :href="route('job_order.others')">
-              <TabsTrigger value="others" class="px-7">
-                Other Services
-              </TabsTrigger>
-            </Link>
-          </TabsList>
-        </Tabs>
-        <JobOrderDataTable
-          :columns="columns"
-          :data="data"
-          :meta="meta" />
+          <Tabs :model-value="route().current()" class="w-full">
+            <TabsList class="flex justify-start">
+              <Link :href="route('job_order.index')">
+                <TabsTrigger value="job_order.index" class="px-7">
+                  All
+                </TabsTrigger>
+              </Link>
+              <Link :href="route('job_order.waste_management')">
+                <TabsTrigger value="waste_management" class="px-7">
+                  Waste Management
+                </TabsTrigger>
+              </Link>
+              <Link :href="route('job_order.it_service')">
+                <TabsTrigger value="job_order.it_service" class="px-7">
+                  IT Services
+                </TabsTrigger>
+              </Link>
+              <Link :href="route('job_order.others')">
+                <TabsTrigger value="job_order.others" class="px-7">
+                  Other Services
+                </TabsTrigger>
+              </Link>
+            </TabsList>
+          </Tabs>
+          <JobOrderDataTable
+            :columns="columns"
+            :data="data"
+            :meta="meta" />
+        </div>        
       </div>
     </AppLayout>
 </template>
