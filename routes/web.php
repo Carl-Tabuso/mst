307cca1a7aeb5b4844018e0c754c8052ab408330
Peter-Escueta <peterwilrexemescueta@iskolarngbayan.pup.edu.ph>
2025-06-29 03:52:29 +0800

    feat: added incident-report routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\IncidentController;
+use App\Models\Employee;
+use App\Models\JobOrder;
 use App\Http\Controllers\ExportJobOrderController;
 use App\Http\Controllers\JobOrderController;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::middleware(['auth'])->group(function () {
     Route::inertia('/', 'Home')->name('home');
@@ -13,12 +18,23 @@ Route::middleware(['auth'])->group(function () {
         Route::post('/', [JobOrderController::class, 'store'])->name('store');
         Route::delete('{jobOrder?}', [JobOrderController::class, 'destroy'])->name('destroy');
         Route::get('export', ExportJobOrderController::class)->name('export');
-
         Route::get('waste-managements', fn () => dd('wm'))->name('waste_management');
         Route::get('it-services', fn () => dd('it'))->name('it_service');
         Route::get('others', fn () => dd('os'))->name('others');
     });
-});
 
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+  Route::prefix('incidents')->name('incidents.')->group(function () {
+        Route::get('report', [IncidentController::class, 'showReport'])->name('report');
+        Route::get('/', [IncidentController::class, 'index'])->name('index');
+        Route::post('/', [IncidentController::class, 'store'])->name('store');
+        Route::patch('{incident}/read', [IncidentController::class, 'markAsRead'])->name('read');
+    });
+
+    Route::prefix('data')->group(function () {
+        Route::get('employees/dropdown', [EmployeeController::class, 'dropdown'])->name('employees.dropdown');
+                Route::get('/job-orders/dropdown', [JobOrderController::class, 'dropdownOptions']);
+
+    });});
+
+require __DIR__ . '/settings.php';
+require __DIR__ . '/auth.php';
