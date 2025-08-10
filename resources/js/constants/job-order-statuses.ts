34f369f88003f05bdb8ba78f9fb96e4add67ae0d
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2025-08-10 21:04:52 +0800

    use correct description

--- resources/js/constants/job-order-statuses.ts
+++ resources/js/constants/job-order-statuses.ts
@@ -91,7 +91,7 @@ export const manualUpdateStatuses: manualStatusUpdaterType[] = [
       label: 'Mark as dropped',
     },
     description:
-      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad reprehenderit aperiam ullam mollitia repellendus dolor dolores vero, laudantium explicabo voluptatibus velit eaque ut odit nihil qui est reiciendis alias ipsam?',
+      'Choose “Mark as viewed” if the business client will proceed to the proposal stage, or “Mark as dropped” if they have decided not to move forward to ocular inspection.',
   },
   {
     id: 'for proposal',
@@ -104,7 +104,7 @@ export const manualUpdateStatuses: manualStatusUpdaterType[] = [
       label: 'Mark as failed',
     },
     description:
-      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad reprehenderit aperiam ullam mollitia repellendus dolor dolores vero, laudantium explicabo voluptatibus velit eaque ut odit nihil qui est reiciendis alias ipsam?',
+      'Choose “Mark as successful if the business client accepted the proposal terms and conditions, otherwise “Mark as failed if they have decided not to move forward.',
   },
   {
     id: 'hauling in-progress',
@@ -117,7 +117,7 @@ export const manualUpdateStatuses: manualStatusUpdaterType[] = [
       label: 'Mark as closed',
     },
     description:
-      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad reprehenderit aperiam ullam mollitia repellendus dolor dolores vero, laudantium explicabo voluptatibus velit eaque ut odit nihil qui est reiciendis alias ipsam?',
+      'Choose “Mark as for verification” if all hauling activities are complete and ready for HR incident report verification, or “Mark as closed” if work cannot proceed due to unforeseen circumstances.',
   },
 ]
 
