04598daf86a2d4e41b724856aa81b21529dff9d8
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2025-08-15 18:34:30 +0800

    wip: regular

--- resources/js/pages/home/3/Index.vue
+++ resources/js/pages/home/3/Index.vue
@@ -1,8 +1,15 @@
 <script setup lang="ts">
 import AppLayout from '@/layouts/AppLayout.vue'
 import { BreadcrumbItem } from '@/types'
+import { GreetingKey } from '..'
+import GreetingIllustration from '../components/GreetingIllustration.vue'
 
-interface Home3Props {}
+interface Home3Props {
+  dayPart: GreetingKey
+  illustration: string
+}
+
+defineProps<Home3Props>()
 
 const breadcrumbs: BreadcrumbItem[] = [
   {
@@ -16,6 +23,19 @@ const breadcrumbs: BreadcrumbItem[] = [
   <Head title="Home" />
 
   <AppLayout :breadcrumbs="breadcrumbs">
-    <div class="mx-auto mb-6 mt-3 w-full max-w-screen-xl px-6">Home 3</div>
+    <div class="mx-auto mb-6 mt-3 w-full max-w-screen-xl px-6">
+      <div>
+        <div class="grid items-start gap-4 md:grid-cols-1 lg:grid-cols-3">
+          <div class="flex flex-col gap-4 lg:col-span-2">
+            <div>
+              <GreetingIllustration
+                :day-part="dayPart"
+                :illustration="illustration"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </AppLayout>
 </template>
