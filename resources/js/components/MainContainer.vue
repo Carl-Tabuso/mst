6d54b7b7df1841a6eede7e532c71652fb1537970
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2025-09-05 21:17:44 +0800

    apply prettier

--- resources/js/components/MainContainer.vue
+++ resources/js/components/MainContainer.vue
@@ -1,7 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { SharedData } from '@/types'
+import { usePage } from '@inertiajs/vue3'
+
+const page = usePage<SharedData>()
+const isFrontliner = page.props.auth.user.roles[0].name === 'head frontliner'
+</script>
 
 <template>
-  <div class="mx-auto mb-6 mt-3 w-full max-w-screen-xl pl-3 pr-6">
+  <div
+    class="mx-auto mb-6 mt-3 w-full max-w-screen-xl"
+    :class="[isFrontliner ? 'pl-3 pr-6' : 'px-6']"
+  >
     <slot />
   </div>
 </template>
