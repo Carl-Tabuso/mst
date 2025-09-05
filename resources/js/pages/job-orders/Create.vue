37dc8cb334fcab8356e67b4781df4761565aa507
Peter-Escueta <peterwilrexemescueta@iskolarngbayan.pup.edu.ph>
2025-09-05 20:09:09 +0800

    feat: Added form5section to conditionally appear on create job order page

--- resources/js/pages/job-orders/Create.vue
+++ resources/js/pages/job-orders/Create.vue
@@ -3,10 +3,14 @@ import { Button } from '@/components/ui/button'
 import { jobOrderRouteNames } from '@/constants/job-order-route'
 import AppLayout from '@/layouts/AppLayout.vue'
 import { type BreadcrumbItem } from '@/types'
-import { useForm } from '@inertiajs/vue3'
+import { useForm, usePage } from '@inertiajs/vue3' 
 import { LoaderCircle } from 'lucide-vue-next'
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import FirstSection from './waste-managements/components/sections/FirstSection.vue'
+import Form5Section from './other-services/components/Form5Section.vue' 
+
+const page = usePage()
+const employees = computed(() => page.props.employees || [])
 
 const form = useForm({
   service_type: 'form4',
@@ -17,20 +21,44 @@ const form = useForm({
   contact_position: '',
   contact_person: '',
   contact_no: '',
+  assigned_person: null,
+  items: [],
+  purpose: '', 
 })
 
 const timeOfService = ref('')
 
+const isForm5 = computed(() => form.service_type === 'form5')
+
 const onSubmit = () => {
+
   const [hours, min] = timeOfService.value.split(':')
   const epoch = new Date(form.date_time).setHours(Number(hours), Number(min))
-  // found this abomination online. I need to format the date to Y-m-d H:i:s
   const formatted = new Date(epoch).toJSON().split('.')[0].split('T').join(' ')
 
-  form.transform((data) => ({
-    ...data,
-    date_time: formatted,
-  }))
+  form.transform((data) => {
+    const base = {
+      ...data,
+      date_time: formatted,
+    }
+
+    if (isForm5.value) {
+      
+      return {
+        ...base,
+        assigned_person: data.assigned_person,
+        items: data.items,
+        purpose: data.purpose,
+      }
+    }
+
+    
+    return {
+      ...base,
+      assigned_person: null,
+      items: [],
+    }
+  })
 
   const path = jobOrderRouteNames.find((j) => j.id === form.service_type)
 
@@ -38,18 +66,9 @@ const onSubmit = () => {
 }
 
 const breadcrumbs: BreadcrumbItem[] = [
-  {
-    title: 'Job Orders',
-    href: '/job-orders',
-  },
-  {
-    title: 'List',
-    href: '/job-orders',
-  },
-  {
-    title: 'Create',
-    href: '#',
-  },
+  { title: 'Job Orders', href: '/job-orders' },
+  { title: 'List', href: '/job-orders' },
+  { title: 'Create', href: '#' },
 ]
 </script>
 
@@ -81,9 +100,16 @@ const breadcrumbs: BreadcrumbItem[] = [
                   v-model:contactPerson="form.contact_person"
                   v-model:contactNumber="form.contact_no"
                 />
-              </div>
 
-              <!-- <Separator class="col-[1/-1] my-4 w-full" /> -->
+                <Form5Section
+                  v-if="isForm5"
+                  v-model:assignedPerson="form.assigned_person"
+                  v-model:items="form.items"
+                  v-model:purpose="form.purpose"
+                  :employees="employees"
+                  :isEditing="true"
+                />
+              </div>
 
               <div class="col-[1/-1] flex w-full items-center">
                 <div class="ml-auto space-x-3">
