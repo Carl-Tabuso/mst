976af077a6f16b15c6b0583298e3119ca7c1e642
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2025-09-11 14:43:54 +0800

    refactor archived job order toolbar

--- resources/js/pages/archives/job-orders/components/DataTableToolbar.vue
+++ resources/js/pages/archives/job-orders/components/DataTableToolbar.vue
@@ -1,37 +1,15 @@
 <script setup lang="ts" generic="TData">
+import ColumnViewToggle from '@/components/ColumnViewToggle.vue'
 import { Badge } from '@/components/ui/badge'
 import { Button } from '@/components/ui/button'
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogTitle,
-  DialogTrigger,
-} from '@/components/ui/dialog'
-import {
-  DropdownMenu,
-  DropdownMenuCheckboxItem,
-  DropdownMenuContent,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
 import { Input } from '@/components/ui/input'
+import { usePermissions } from '@/composables/usePermissions'
 import { JobOrder } from '@/types'
-import { router } from '@inertiajs/vue3'
 import type { Table } from '@tanstack/vue-table'
 import { useDebounceFn } from '@vueuse/core'
-import {
-  ArchiveRestore,
-  Download,
-  LoaderCircle,
-  Search,
-  Settings2,
-} from 'lucide-vue-next'
-import { VisuallyHidden } from 'radix-vue'
-import { computed, ref } from 'vue'
-import { toast } from 'vue-sonner'
+import { Download, Search } from 'lucide-vue-next'
+import { computed } from 'vue'
+import BulkRestoreJobOrder from './BulkRestoreJobOrder.vue'
 import FilterPopover from './FilterPopover.vue'
 
 interface DataTableToolbarProps {
@@ -41,57 +19,21 @@ interface DataTableToolbarProps {
 
 const props = defineProps<DataTableToolbarProps>()
 
-const visibleColumnCount = computed(
-  () =>
-    props.table
-      .getAllColumns()
-      .filter((column) => column.getCanHide() && column.getIsVisible()).length,
-)
-
 const onSearch = useDebounceFn((value: string | number) => {
-  console.log('hello')
   props.table.setGlobalFilter(value)
 }, 500)
 
 const selectedRowModels = computed(() => props.table.getSelectedRowModel().rows)
 
-const jobOrderIds = computed(() => {
-  return selectedRowModels.value.map((row) => row.original.id)
-})
-
 const onExport = () => {
   window.open(
     route('job_order.export', {
-      jobOrderIds: jobOrderIds.value,
+      jobOrderIds: selectedRowModels.value.map((row) => row.original.id),
     }),
     '_blank',
   )
 
   props.table.resetRowSelection()
-}
-
-const isSubmitting = ref<boolean>(false)
-
-const onBulkRestore = () => {
-  router.patch(
-    route('archive.job_order.bulk_restore'),
-    {
-      jobOrderIds: jobOrderIds.value,
-    },
-    {
-      preserveScroll: true,
-      onBefore: () => (isSubmitting.value = true),
-      onSuccess: (page: any) => {
-        toast.success(page.props.flash.message, {
-          position: 'top-center',
-        })
-      },
-      onFinish: () => {
-        isSubmitting.value = false
-        props.table.resetRowSelection()
-      },
-    },
-  )
 }
 </script>
 
@@ -113,37 +55,7 @@ const onBulkRestore = () => {
       </span>
     </div>
     <FilterPopover />
-    <DropdownMenu>
-      <DropdownMenuTrigger as-child>
-        <Button
-          variant="ghost"
-          class="mx-1"
-        >
-          <Settings2 class="mr-2" />
-          View
-          <div class="hidden space-x-1 lg:flex">
-            <Badge
-              variant="secondary"
-              class="rounded-full font-extrabold"
-            >
-              {{ visibleColumnCount }}
-            </Badge>
-          </div>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <DropdownMenuLabel>Toggle columns</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuCheckboxItem
-          v-for="column in table.getAllColumns().filter((c) => c.getCanHide())"
-          :key="column.id"
-          :checked="column.getIsVisible()"
-          @update:checked="(value: boolean) => column.toggleVisibility(value)"
-        >
-          {{ column.columnDef.meta?.label }}
-        </DropdownMenuCheckboxItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <ColumnViewToggle :table="table" />
     <Button
       @click="onExport"
       variant="ghost"
@@ -160,69 +72,13 @@ const onBulkRestore = () => {
       </Badge>
     </Button>
     <div
-      v-if="selectedRowModels.length"
+      v-if="
+        selectedRowModels.length &&
+        usePermissions().can('restore:archived_job_order')
+      "
       class="ml-auto"
     >
-      <Dialog>
-        <DialogTrigger as-child>
-          <Button class="mx-3">
-            <ArchiveRestore class="mr-2" />
-            Restore
-            <span class="hidden pl-3 font-extrabold lg:flex">
-              {{ selectedRowModels.length }}
-            </span>
-          </Button>
-        </DialogTrigger>
-        <DialogContent class="w-fit">
-          <VisuallyHidden as-child>
-            <DialogTitle>Restoring Job Orders</DialogTitle>
-            <DialogDescription>
-              <!---->
-            </DialogDescription>
-          </VisuallyHidden>
-          <div class="flex flex-col items-center justify-center gap-2">
-            <ArchiveRestore class="h-28 w-28" />
-            <div class="text-3xl font-bold">
-              Restoring {{ selectedRowModels.length }} Job Order(s)
-            </div>
-            <div class="text-sm text-muted-foreground">
-              Are you sure you want to restore the following?
-            </div>
-            <div class="max-h-40 w-full overflow-y-auto">
-              <ul class="grid gap-2">
-                <li
-                  v-for="row in selectedRowModels"
-                  :key="row.id"
-                  class="rounded-sm bg-muted px-3 py-2 text-xs font-medium shadow-sm"
-                >
-                  {{ row.getValue('ticket') }}
-                </li>
-              </ul>
-            </div>
-            <div class="mt-4 flex flex-row items-center gap-4">
-              <DialogClose>
-                <Button
-                  variant="outline"
-                  class="px-10"
-                >
-                  Cancel
-                </Button>
-              </DialogClose>
-              <Button
-                :disabled="isSubmitting"
-                @click="onBulkRestore"
-                class="px-10"
-              >
-                <LoaderCircle
-                  v-if="isSubmitting"
-                  class="mr-2 animate-spin"
-                />
-                Confirm
-              </Button>
-            </div>
-          </div>
-        </DialogContent>
-      </Dialog>
+      <BulkRestoreJobOrder :table="table" />
     </div>
   </div>
 </template>
