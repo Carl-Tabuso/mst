a2693f098b77105e840bcc9f4286e8862609e24f
Peter-Escueta <peterwilrexemescueta@iskolarngbayan.pup.edu.ph>
2025-09-11 12:48:31 +0800

    new resource files

---
+++ app/Http/Resources/EmployeeCompensationResource.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class EmployeeCompensationResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'employeeId' => $this->employee_id,
+            'salary' => $this->salary,
+            'allowance' => $this->allowance,
+        ];
+    }
+}
---
+++ app/Http/Resources/EmployeeEmergencyContactResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class EmployeeEmergencyContactResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'employeeId' => $this->employee_id,
+            'lastName' => $this->last_name,
+            'firstName' => $this->first_name,
+            'middleName' => $this->middle_name,
+            'suffix' => $this->suffix,
+            'contactNumber' => $this->contact_number,
+            'relation' => $this->relation,
+        ];
+    }
+}
