c7deac32bfbefbea9055b043c997eb11fe4e0da4
Peter-Escueta <peterwilrexemescueta@iskolarngbayan.pup.edu.ph>
2025-09-14 05:20:48 +0800

    added extend to verifyemail to include password and email with it

---
+++ app/Notifications/VerifyEmailWithPassword.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class VerifyEmailWithPassword extends VerifyEmail implements ShouldQueue
+{
+    use Queueable;
+
+    public $password;
+
+    public function __construct($password)
+    {
+        $this->password = $password;
+    }
+
+    public function toMail($notifiable)
+    {
+        $verificationUrl = $this->verificationUrl($notifiable);
+
+        return (new MailMessage)
+            ->subject('Verify Email Address - ' . config('app.name'))
+            ->line('Your account has been created successfully!')
+            ->line('Here are your login credentials:')
+            ->line('**Email:** ' . $notifiable->email)
+            ->line('**Password:** ' . $this->password)
+            ->line('Please click the button below to verify your email address.')
+            ->action('Verify Email Address', $verificationUrl)
+            ->line('After verifying, we recommend changing your password on first login.')
+            ->line('If you did not create an account, no further action is required.');
+    }
+}
