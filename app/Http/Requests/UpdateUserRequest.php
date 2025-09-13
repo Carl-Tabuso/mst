2d48b54b4945ca11d146af7e7c68556d9f13fc99
Peter-Escueta <peterwilrexemescueta@iskolarngbayan.pup.edu.ph>
2025-09-14 05:21:17 +0800

    New requests for user

---
+++ app/Http/Requests/UpdateUserRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateUserRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email,' . $this->route('user')->id,
+        ];
+    }
+}
