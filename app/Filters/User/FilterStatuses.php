a3b61b059b8e4a3bae7cbb9ba95576d2a3f2d99a
Peter-Escueta <peterwilrexemescueta@iskolarngbayan.pup.edu.ph>
2025-09-14 05:21:05 +0800

    user table filtering

---
+++ app/Filters/User/FilterStatuses.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Filters\User;
+
+use Illuminate\Database\Eloquent\Builder;
+
+class FilterStatuses
+{
+    public function __construct(protected array $statuses = []) {}
+
+    public function handle(Builder $query, $next)
+    {
+        if (empty($this->statuses)) {
+            return $next($query);
+        }
+
+        $query->where(function (Builder $query) {
+            if (in_array('active', $this->statuses)) {
+                $query->orWhereNull('deleted_at');
+            }
+            
+            if (in_array('inactive', $this->statuses)) {
+                $query->orWhereNotNull('deleted_at');
+            }
+        });
+
+        return $next($query);
+    }
+}
