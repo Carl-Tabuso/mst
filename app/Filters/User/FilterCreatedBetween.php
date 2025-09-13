63dc70946c7151288d977705b8909cf5ffd94a1f
Peter-Escueta <peterwilrexemescueta@iskolarngbayan.pup.edu.ph>
2025-09-14 05:21:05 +0800

    user table filtering

---
+++ app/Filters/User/FilterCreatedBetween.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Filters\User;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Carbon;
+
+class FilterCreatedBetween
+{
+    public function __construct(protected ?string $fromDate, protected ?string $toDate) {}
+
+    public function handle(Builder $query, $next)
+    {
+        if (blank($this->fromDate) && blank($this->toDate)) {
+            return $next($query);
+        }
+
+        $query->where(function (Builder $query) {
+            if (!blank($this->fromDate)) {
+                $query->whereDate('created_at', '>=', Carbon::parse($this->fromDate));
+            }
+
+            if (!blank($this->toDate)) {
+                $query->whereDate('created_at', '<=', Carbon::parse($this->toDate));
+            }
+        });
+
+        return $next($query);
+    }
+}
