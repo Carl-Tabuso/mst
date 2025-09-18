6238ecbbc3c0da16ededddb2ea999904a5aa7b7b
Peter-Escueta <peterwilrexemescueta@iskolarngbayan.pup.edu.ph>
2025-09-19 02:40:20 +0800

    removed employee dropdown function

--- app/Services/EmployeeService.php
+++ app/Services/EmployeeService.php
@@ -238,15 +238,4 @@ class EmployeeService
     {
         return $employee->forceDelete();
     }
-
-    public function getEmployeesForDropdown()
-    {
-        return Employee::select('id', 'first_name', 'last_name')
-            ->orderBy('last_name')
-            ->get()
-            ->map(fn ($employee) => [
-                'id'   => $employee->id,
-                'name' => "{$employee->first_name} {$employee->last_name}",
-            ]);
-    }
 }
