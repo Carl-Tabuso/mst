04256be037252d17512ba73f7640eb6a9888f472
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2025-09-18 17:47:56 +0800

    valid filter and ensure each user will have a role

--- database/seeders/ArchivedUserSeeder.php
+++ database/seeders/ArchivedUserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Enums\UserRole;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\DB;
 
 class ArchivedUserSeeder extends Seeder
@@ -13,12 +14,16 @@ class ArchivedUserSeeder extends Seeder
     {
         activity()->disableLogging();
 
-        DB::transaction(function () {
+        $validRoles = array_diff(array_map(
+            fn ($role) => $role->value, UserRole::cases()
+        ), [UserRole::HeadFrontliner->value]);
+
+        DB::transaction(function () use ($validRoles) {
             User::factory()
                 ->count(20)
                 ->trashed()
                 ->create()
-                ->each(fn (User $user) => $user->assignRole(array_rand(UserRole::cases())));
+                ->each(fn (User $user) => $user->assignRole(Arr::random($validRoles)));
         });
     }
 }
