ea551f4598d741919addd8d2edf3d54cfb15586b
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2025-09-25 13:23:01 +0800

    add breadcrumbs and cleanup

--- resources/js/components/AppHeader.vue
+++ resources/js/components/AppHeader.vue
@@ -21,12 +21,6 @@ import {
   SheetTitle,
   SheetTrigger,
 } from '@/components/ui/sheet'
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from '@/components/ui/tooltip'
 import UserMenuContent from '@/components/UserMenuContent.vue'
 import { usePermissions } from '@/composables/usePermissions'
 import { UserRoleType } from '@/constants/user-role'
@@ -50,6 +44,7 @@ import {
 import { computed } from 'vue'
 import DarkModeToggle from './DarkModeToggle.vue'
 import UserAvatar from './UserAvatar.vue'
+import Breadcrumbs from './Breadcrumbs.vue'
 
 interface Props {
   breadcrumbs?: BreadcrumbItem[]
@@ -172,19 +167,6 @@ const mainNavItems: NavItem[] = [
     can: true,
   },
 ]
-
-const rightNavItems: NavItem[] = [
-  // {
-  //   title: 'Repository',
-  //   href: 'https://github.com/laravel/vue-starter-kit',
-  //   icon: Folder,
-  // },
-  // {
-  //   title: 'Documentation',
-  //   href: 'https://laravel.com/docs/starter-kits',
-  //   icon: BookOpen,
-  // },
-]
 </script>
 
 <template>
@@ -236,23 +218,6 @@ const rightNavItems: NavItem[] = [
                     </Link>
                   </template>
                 </nav>
-                <div class="flex flex-col space-y-4">
-                  <a
-                    v-for="item in rightNavItems"
-                    :key="item.title"
-                    :href="item.href"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    class="flex items-center space-x-2 text-sm font-medium"
-                  >
-                    <component
-                      v-if="item.icon"
-                      :is="item.icon"
-                      class="h-5 w-5"
-                    />
-                    <span>{{ item.title }}</span>
-                  </a>
-                </div>
               </div>
             </SheetContent>
           </Sheet>
@@ -299,49 +264,7 @@ const rightNavItems: NavItem[] = [
 
         <div class="ml-auto flex items-center space-x-2">
           <div class="relative flex items-center space-x-1">
-            <!-- <Button
-              variant="ghost"
-              size="icon"
-              class="group h-9 w-9 cursor-pointer"
-            >
-              <Search class="size-5 opacity-80 group-hover:opacity-100" />
-            </Button> -->
             <DarkModeToggle />
-
-            <div class="hidden space-x-1 lg:flex">
-              <template
-                v-for="item in rightNavItems"
-                :key="item.title"
-              >
-                <TooltipProvider :delay-duration="0">
-                  <Tooltip>
-                    <TooltipTrigger>
-                      <Button
-                        variant="ghost"
-                        size="icon"
-                        as-child
-                        class="group h-9 w-9 cursor-pointer"
-                      >
-                        <a
-                          :href="item.href"
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          <span class="sr-only">{{ item.title }}</span>
-                          <component
-                            :is="item.icon"
-                            class="size-5 opacity-80 group-hover:opacity-100"
-                          />
-                        </a>
-                      </Button>
-                    </TooltipTrigger>
-                    <TooltipContent>
-                      <p>{{ item.title }}</p>
-                    </TooltipContent>
-                  </Tooltip>
-                </TooltipProvider>
-              </template>
-            </div>
           </div>
 
           <DropdownMenu>
@@ -368,16 +291,16 @@ const rightNavItems: NavItem[] = [
         </div>
       </div>
     </div>
-
-    <!-- <div
-      v-if="props.breadcrumbs.length > 1"
+    <div
+      v-if="breadcrumbs.length > 1"
       class="flex w-full"
     >
       <div
-        class="mx-auto flex h-12 w-full items-center justify-start px-6 text-neutral-500 md:max-w-7xl"
+        class="mx-auto flex h-12 w-full items-center justify-start px-6 text-muted-foreground md:max-w-7xl"
       >
         <Breadcrumbs :breadcrumbs="breadcrumbs" />
       </div>
-    </div> -->
+    </div>
+    <div v-else class="mb-3" />
   </div>
 </template>
