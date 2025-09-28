bbbcd874fd4f2498f901b04f4e797c714cd736b6
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2025-09-28 17:21:00 +0800

    remove stale files

--- database/seeders/ProductionDatabaseSeeder.php
+++ database/seeders/ProductionDatabaseSeeder.php
@@ -4,11 +4,16 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 
 class ProductionDatabaseSeeder extends Seeder
 {
     public function run(): void
     {
+        Storage::disk('public')->deleteDirectory('avatars');
+        Storage::disk('local')->deleteDirectory('it_services/reports');
+        Storage::disk('local')->deleteDirectory('it_services/temp');
+
         DB::transaction(function () {
             $this->call([
                 PositionSeeder::class,
