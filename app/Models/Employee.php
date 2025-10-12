e5b240532daa36dc59c1edf52d6db585fa31aaa5
hatdogguldo <redondoblessed@gmail.com>
2025-10-12 18:29:54 +0800

    fix: adjust the profile also

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -189,9 +189,20 @@ class Employee extends Model
         return $this->hasMany(Form3AssignedPersonnel::class, 'team_leader');
     }
 
-    public function assignedPersonnelAsDriver(): HasMany
+    // we can remove this @carl
+    // public function assignedPersonnelAsDriver(): HasMany
+    // {
+    //     return $this->hasMany(Form3AssignedPersonnel::class, 'team_driver');
+    // }
+
+    public function form3sDriver(): BelongsToMany
     {
-        return $this->hasMany(Form3AssignedPersonnel::class, 'team_driver');
+        return $this->belongsToMany(
+            Form3Hauling::class,
+            'form3_drivers',
+            'driver',
+            'form3_hauling_id'
+        );
     }
 
     public function assignedPersonnelAsSafetyOfficer(): HasMany
